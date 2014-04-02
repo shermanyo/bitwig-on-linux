548e422de65d421866d9e1853e46d1f763ff0058
shermanyo <bensherman42@gmail.com>
2014-04-03 01:28:16 +1000

    Create logsrv.js

---
+++ logsrv.js
@@ -0,0 +1,34 @@
+// logsrv.js
+
+var net = require('net');
+var fs = require('fs');
+var tmpfilepath = '/tmp/logsrv.txt';
+
+// Create our socket
+var server = net.createServer(
+  function (socket) {
+
+    // Callback when data is received
+    socket.on('data', function(data) {
+
+      var messagetxt = "";
+
+      // Convert bytes to string      for(var i = 0; i < data.length; i++) {
+        messagetxt += String.fromCharCode(data[i]);
+      }
+
+      // Print message to console
+      console.log(messagetxt);
+
+      // Write message to file
+      fs.writeFile(tmpfilepath, messagetxt, function(err) {
+        if(err) { console.error("Write error: %s", err); }
+      });
+
+    });
+  }
+
+);
+
+// Start server
+server.listen(58008, '127.0.0.1');
